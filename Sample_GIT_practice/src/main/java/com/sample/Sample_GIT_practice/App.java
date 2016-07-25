49d67fbbdbaaf3980a520308813ea206d5c5d0bf
shankarrao.kota <shankarrao.kota@CITHYDWIN8-022.CaprusIT.local>
2016-07-25 14:48:59 +0530

    committed to branch with syso

--- Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
+++ Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
@@ -6,9 +6,12 @@ package com.sample.Sample_GIT_practice;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("branch is deleted   second time (remote tracking)");
+        
+        System.out.println("This is data in child branch ");
     }
 }
