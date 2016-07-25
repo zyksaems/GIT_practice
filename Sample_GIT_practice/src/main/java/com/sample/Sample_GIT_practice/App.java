5a247a6f9bbe9a407b73a65d0177f7b50ac1ce84
shankarrao.kota <shankarrao.kota@CITHYDWIN8-022.CaprusIT.local>
2016-07-25 13:04:02 +0530

    commit by indexing conflict

--- Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
+++ Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
@@ -1,14 +1,14 @@
 package com.sample.Sample_GIT_practice;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
         System.out.println("branch is deleted   second time (remote tracking)");
+        
+        System.out.println("for conflict ");
     }
 }
