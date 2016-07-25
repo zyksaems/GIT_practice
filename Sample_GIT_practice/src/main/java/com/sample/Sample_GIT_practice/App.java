9caf148e4c77ea6f618c928c327e3101ff879a12
shankarrao.kota <shankarrao.kota@CITHYDWIN8-022.CaprusIT.local>
2016-07-25 12:29:18 +0530

    commiited by addig cherryPick_1 class

---
+++ Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
@@ -0,0 +1,14 @@
+package com.sample.Sample_GIT_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("branch is deleted");
+    }
+}
