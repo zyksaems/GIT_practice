1c1d867e65101ad0e4f3cfc47275d22e0b8f53fc
shankarrao.kota <shankarrao.kota@CITHYDWIN8-022.CaprusIT.local>
2016-07-25 12:10:41 +0530

    This is second commit into branch_one

---
+++ Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/Branch_one.java
@@ -0,0 +1,14 @@
+package com.sample.Sample_GIT_practice;
+
+public class Branch_one {
+	
+	private int secondClassInbranch;
+
+	@Override
+	public String toString() {
+		return "Branch_one [secondClassInbranch=" + secondClassInbranch + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+
+}
