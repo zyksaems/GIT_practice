0d52ceb7b75c29007605e7b3707b296b78b3a76c
shankarrao.kota <shankarrao.kota@CITHYDWIN8-022.CaprusIT.local>
2016-07-25 12:32:07 +0530

    commited by deleting branch in remote

--- Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
+++ Sample_GIT_practice/src/main/java/com/sample/Sample_GIT_practice/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("branch is deleted");
+        System.out.println("branch is deleted   second time (remote tracking)");
     }
 }
